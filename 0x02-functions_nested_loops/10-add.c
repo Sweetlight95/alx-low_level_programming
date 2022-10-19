b114ab057a5832d1bc685588166baea1f1e29f5d
Sweetlight95 <princessadesuwa93@gmail.com>
2022-10-19 16:04:17 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* add- adds integers
+* @n1: integer 1
+* @n2: ineger 2
+* Description: sums
+* Return: sum
+*/
+
+int add(int n1, int n2)
+{
+	int sum;
+
+	sum = n1 + n2;
+	return (sum);
+}
