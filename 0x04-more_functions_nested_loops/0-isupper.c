a83b750697cb82ef5e07ff1fc79644e8a4072dbc
Sweetlight95 <princessadesuwa93@gmail.com>
2022-10-20 16:31:00 -0700

    0task

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,16 +3,12 @@
 /**
 * _isupper - checks if a character is uppercase
 * @c: Variable text
-* Return: Always 0
+* Return: Always 0 or 1
 */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-	{
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
